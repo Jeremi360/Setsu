4b493d5d40c36ffc21c5069ebde226b07c546926
Jeremi Biernacki <jeremi360@gmail.com>
2024-09-13 21:28:23 +0200

    crate base RakuNodeInspector class/scene

---
+++ scenes/Inspector/raku_inspector.gd
@@ -0,0 +1,10 @@
+class_name RakuNodeInspector
+extends VBoxContainer
+
+@export var type := RakuNode.Unknown
+
+func from_raku(expression: String) -> void:
+	pass
+
+func update_node() -> void:
+	pass
