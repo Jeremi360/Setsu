6fcda9323ca81b51de03cddca5038694f9bfcb3d
Jeremi Biernacki <jeremi360@gmail.com>
2024-10-10 12:29:04 +0200

    Init RDS plugin

--- scripts/SetsuNodeDef.gd
+++ scripts/SetsuNodeDef.gd
@@ -1,8 +1,17 @@
 class_name SetsuNodeDefintion
 extends Resource
 
-@export var name := "TestPlugin"
-@export var icon : ImageTexture
-@export var style : StyleBox
-@export var graph_node : PackedScene
-@export var inspector_node : PackedScene
+@export var name := "NodeName"
+@export var icon: Texture
+
+## Styles
+@export_group("Styles", "style_")
+@export var style_normal: StyleBox
+@export_group("Styles", "style_")
+@export var style_selected: StyleBox
+
+## Nodes
+@export_group("Nodes", "node_")
+@export var node_graph: PackedScene
+@export_group("Nodes", "node_")
+@export var node_inspector: PackedScene
